6f283e1fb28526371aa3113738073fa7a91e23c9
André Moabson <amsr@ic.ufal.br>
2018-05-20 22:13:41 -0300

    textures front and back house done

--- src/house.cpp
+++ src/house.cpp
@@ -57,11 +57,37 @@ House::House()
     shapes.push_back(wb.set_height(-2.0f,5.0f)->set_first_point(-0.6f,7.5f)->set_second_point(-5.0f,7.5f)->build());
     wb.reset_builder();
 
-    GLuint textures[6] = {1,1,1,1,1,1};
+    GLuint texturesTop[6] = {
+        15, // 0 front
+        6, // 1 back
+        17, // 2 left
+        17, // 3 right
+        17, // 4 up
+        17  // 5 bottom
+    };
+
+    GLuint texturesLeft[6] = {
+        14, // 0 front
+        18, // 1 back
+        17, // 2 left
+        17, // 3 right
+        17, // 4 up
+        17  // 5 bottom
+    };
+
+    GLuint texturesRight[6] = {
+        16, // 0 front
+        19, // 1 back
+        17, // 2 left
+        17, // 3 right
+        17, // 4 up
+        17  // 5 bottom
+    };
+
     // front wall
-    shapes.push_back(new Parallelepiped(2.8f, -2.0f, -7.5f, 4.4f, 3.0f, 0.1f,textures));
-    shapes.push_back(new Parallelepiped(-2.8f, -2.0f, -7.5f, 4.4f, 3.0f, 0.1f,textures));
-    shapes.push_back(new Parallelepiped(0.0f, 1.0f, -7.5f, 10.0f, 4.0f, 0.1f,textures));
+    shapes.push_back(new Parallelepiped(2.8f, -2.0f, -7.5f, 4.4f, 3.0f, 0.1f, texturesLeft));
+    shapes.push_back(new Parallelepiped(-2.8f, -2.0f, -7.5f, 4.4f, 3.0f, 0.1f, texturesRight));
+    shapes.push_back(new Parallelepiped(0.0f, 1.0f, -7.5f, 10.0f, 4.0f, 0.1f, texturesTop));
 
     // first wall inside the house
     shapes.push_back(wb.set_height(-2.0f,2.0f)->set_first_point(1.4f,-0.6f)->set_second_point(1.4f,-6.0f)->build());
